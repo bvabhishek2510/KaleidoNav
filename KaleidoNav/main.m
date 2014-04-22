1cde9b933268544170086ad5ed4608f6ebb1b567
is-Lib <is-lib@ksc-lab-7.labs.kent.edu>
2014-04-21 21:11:25 -0400

    Initial Commit

---
+++ KaleidoNav/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KaleidoNav
+//
+//  Created by is-Lib on 4/21/14.
+//  Copyright (c) 2014 is-Lib. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "rchelikaAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([rchelikaAppDelegate class]));
+    }
+}
---
+++ KaleidoNav/rchelikaAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  rchelikaAppDelegate.h
+//  KaleidoNav
+//
+//  Created by is-Lib on 4/21/14.
+//  Copyright (c) 2014 is-Lib. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface rchelikaAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
